f8193abff5a255b97d1f6be20e4822eba7cbe9cd
Sparrow <psmd.iberutaru@gmail.com>
2021-09-21 18:16:01 -1000

    Breif experimentation in GUIs

---
+++ gui/primary.py
@@ -0,0 +1,61 @@
+import PySimpleGUIQt as sg
+
+sg.theme('DarkAmber')   # Add a little color to your windows
+# All the stuff inside your window. This is the PSG magic code compactor...
+layout = [  [sg.Text('Some text on Row 1')],
+            [sg.Text('Enter something on Row 2'), sg.InputText(key="-input-")],
+            [sg.Button("Dome"), sg.Text("On",key="-dome-"), sg.Text("He")],
+            [sg.OK(), sg.Cancel()]]
+
+# Create the Window
+window = sg.Window('Window Title', layout)
+dome_state = True
+# Event Loop to process "events"
+while True:             
+    event, values = window.read()
+    print(event, "=", values)
+    if event in (sg.WIN_CLOSED, 'Cancel'):
+        break
+    if event in "Dome":
+        dome_state = not dome_state
+        if dome_state:
+            window["-dome-"].update(text_color="blue")
+        else:
+            window["-dome-"].update(text_color="red")
+
+window.close()
+
+
+
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
+
+class Annotate(object):
+    def __init__(self):
+        self.ax = plt.gca()
+        self.rect = Rectangle((0,0), 1, 1)
+        self.x0 = None
+        self.y0 = None
+        self.x1 = None
+        self.y1 = None
+        self.ax.add_patch(self.rect)
+        self.ax.figure.canvas.mpl_connect('button_press_event', self.on_press)
+        self.ax.figure.canvas.mpl_connect('button_release_event', self.on_release)
+
+    def on_press(self, event):
+        print('press')
+        self.x0 = event.xdata
+        self.y0 = event.ydata
+
+    def on_release(self, event):
+        print('release')
+        self.x1 = event.xdata
+        self.y1 = event.ydata
+        self.rect.set_width(self.x1 - self.x0)
+        self.rect.set_height(self.y1 - self.y0)
+        self.rect.set_xy((self.x0, self.y0))
+        self.ax.figure.canvas.draw()
+
+a = Annotate()
+plt.show()
+print(a.x0,a.x1)
