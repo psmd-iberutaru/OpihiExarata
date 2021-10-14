5655f877ac26e625fda5d359eafda322763731a1
Sparrow <psmd.iberutaru@gmail.com>
2021-10-13 17:12:29 -1000

    Adding JSON handling, helpful for webrequests.

--- src/opihiexarata/library/__init__.py
+++ src/opihiexarata/library/__init__.py
@@ -2,5 +2,6 @@
 
 import opihiexarata.library.configuration as config
 import opihiexarata.library.error as error
+import opihiexarata.library.json as json
 import opihiexarata.library.pathname as path
 import opihiexarata.library.typehints as hint
---
+++ src/opihiexarata/library/json.py
@@ -0,0 +1,36 @@
+"""A collection of functions to deal with JSON input and handling. For the 
+most part, these functions are just wrappers around the built-in JSON handling."""
+
+import json
+
+def dictionary_to_json(dictionary:dict) -> str:
+    """Converts a Python dictionary to a JSON string. 
+    
+    Parameters
+    ----------
+    dictionary : dict
+        The Python dictionary which will be converted to a JSON string.
+    
+    Returns
+    -------
+    json_string : str
+        The JSON string.
+    """
+    json_string = json.dump(dictionary)
+    return json_string
+
+def json_to_dictionary(json_string:str) -> dict:
+    """Converts a JSON string to a dictionary.
+    
+    Parameters
+    -------
+    json_string : str
+        The JSON string.
+    
+    Returns
+    ----------
+    dictionary : dict
+        The Python dictionary which will be converted to a JSON string.
+    """
+    dictionary = json.loads(json_string)
+    return dict(dictionary)
