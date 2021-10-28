8ecdf321dd78a0a1cd8974df3bbbc71b0d08331b
Sparrow <psmd.iberutaru@gmail.com>
2021-10-27 19:15:19 -1000

    Adding fits file functionality.

---
+++ src/opihiexarata/library/fits.py
@@ -0,0 +1,75 @@
+"""Fits file based operations. These are kind of like convince functions."""
+
+import numpy as np
+import astropy.io.fits as ap_fits
+import astropy.table as ap_table
+
+import opihiexarata.library as library
+import opihiexarata.library.error as error
+import opihiexarata.library.hint as hint
+
+
+def read_fits_image_file(
+    filename: str, extension: hint.Union[int, str] = 0
+) -> tuple[hint.Header, hint.ArrayLike]:
+    """This reads fits files, assuming that the fits file is an image. It is a
+    wrapper function around the astropy functions.
+
+    Parameters
+    ----------
+    filename : string
+        The filename that the fits image file is at.
+    extension : int or string, default = 0
+        The fits extension that is desired to be opened.
+
+    Returns
+    -------
+    header : Astropy Header
+        The header of the fits file.
+    data : array-like
+        The data image of the fits file.
+    """
+    with ap_fits.open(filename) as hdul:
+        hdu = hdul[extension].copy()
+        header = hdu.header
+        data = hdu.data
+    # Check that the data really is an image.
+    if not isinstance(data, np.ndarray):
+        raise error.FileError(
+            "This function is designed to read image fits files, and thus the data of"
+            " this fits file or extension is expected to be array-like."
+        )
+    return header, data
+
+
+def read_fits_table_file(
+    filename: str, extension: hint.Union[int, str] = 0
+) -> tuple[hint.Header, hint.Table]:
+    """This reads fits files, assuming that the fits file is a binary table.
+    It is a wrapper function around the astropy functions.
+
+    Parameters
+    ----------
+    filename : string
+        The filename that the fits image file is at.
+    extension : int or string, default = 0
+        The fits extension that is desired to be opened.
+
+    Returns
+    -------
+    header : Astropy Header
+        The header of the fits file.
+    table : array-like
+        The data table of the fits file.
+    """
+    with ap_fits.open(filename) as hdul:
+        hdu = hdul[extension].copy()
+        header = hdu.header
+        table = hdu.data
+    # Check that the data really is an image.
+    if not isinstance(table, ap_table.Table):
+        raise error.FileError(
+            "This function is designed to read binary table fits files, and thus the"
+            " data of this fits file or extension is expected to be array-like."
+        )
+    return header, table
--- src/opihiexarata/library/hint.py
+++ src/opihiexarata/library/hint.py
@@ -3,11 +3,16 @@ is for just uniform hinting types.
 
 This should only be used for types which are otherwise not native and would
 require an import, including the typing module. The whole point of this is to
-be a central collection of types for the purpose of type hinting..
+be a central collection of types for the purpose of type hinting.
 """
 
 from typing import *
 from collections import *
 from collections.abc import *
 
+from astropy.io.fits import Header
+from astropy.table import Table
 from numpy.typing import ArrayLike, DTypeLike
+
+# Importing the engines.
+from opihiexarata.library.engine import *
