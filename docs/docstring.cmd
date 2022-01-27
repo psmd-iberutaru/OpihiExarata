99ebac7dfb74148f65e50b0ac923e3ce83fa344e
Sparrow <psmd.iberutaru@gmail.com>
2022-01-27 12:36:20 -1000

    Mistake on doc testing.

--- docs/docstring.cmd
+++ docs/docstring.cmd
@@ -1,7 +1,7 @@
 :;# This is a special script which intermixes both sh and cmd code. It is 
 :;# written this way because it is used in system() shell-outs directly in 
 :;# otherwise portable code. See https://stackoverflow.com/questions/17510688
-:;# for details.
+:;# for details. (Must be LF line ending for Linux compatability.)
 
 
 :;# Execute the building of the Python document strings, toggling based on how
