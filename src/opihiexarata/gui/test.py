89fc501126443140c5376fe6ec40e7783dc4f195
Sparrow <psmd.iberutaru@gmail.com>
2022-04-04 23:10:24 -1000

    Everything else...

--- src/opihiexarata/gui/test.py
+++ src/opihiexarata/gui/test.py
@@ -1,62 +1,46 @@
 import sys
-from PyQt5.QtWidgets import QDialog, QApplication, QPushButton, QVBoxLayout
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PIL import ImageQt
 
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
-import matplotlib.pyplot as plt
 
-import random
+class getColorLabel(QLabel):
+    def __init__(self, widget):
+        super(getColorLabel, self).__init__(widget)
+        self.main = widget
 
-class Window(QDialog):
-    def __init__(self, parent=None):
-        super(Window, self).__init__(parent)
+    def mousePressEvent(self, event):
+        self.main.get(event.pos())
 
-        # a figure instance to plot on
-        self.figure = plt.figure()
 
-        # this is the Canvas Widget that displays the `figure`
-        # it takes the `figure` instance as a parameter to __init__
-        self.canvas = FigureCanvas(self.figure)
+class MainWindow(QWidget):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.resize(800, 600)
+        self.setWindowTitle('Drop Event Test')
 
-        # this is the Navigation widget
-        # it takes the Canvas widget and a parent
-        self.toolbar = NavigationToolbar(self.canvas, self)
+        # Get Position
+        self.label = getColorLabel(self)
+        self.label.setGeometry(200, 250, 400, 300)
+        self.label.setPixmap(QPixmap('test.png'))
+        self.label.setScaledContents(True)
 
-        # Just some button connected to `plot` method
-        self.button = QPushButton('Plot')
-        self.button.clicked.connect(self.plot)
+        # Display
+        self.displayLabel = QLabel(self)
+        self.displayLabel.setGeometry(300, 0, 200, 200)
+        self.displayLabel.setStyleSheet('background-color: rgb(255, 255, 255);')
 
-        # set the layout
-        layout = QVBoxLayout()
-        layout.addWidget(self.toolbar)
-        layout.addWidget(self.canvas)
-        layout.addWidget(self.button)
-        self.setLayout(layout)
+    def get(self, pos):
+        index = pos.y()*self.label.size().width()+pos.x()
+        image = ImageQt.fromqpixmap(self.label.pixmap())
+        image = image.resize((self.label.size().width(), self.label.size().height()))
+        image_data = image.getdata()
+        r, g, b = image_data[index]
+        self.displayLabel.setStyleSheet('background-color: rgb({},{},{});'.format(r, g, b))
 
-    def plot(self):
-        ''' plot some random stuff '''
-        # random data
-        data = [random.random() for i in range(10)]
-
-        # instead of ax.hold(False)
-        self.figure.clear()
-
-        # create an axis
-        ax = self.figure.add_subplot(111)
-
-        # discards the old graph
-        # ax.hold(False) # deprecated, see above
-
-        # plot data
-        ax.plot(data, '*-')
-
-        # refresh canvas
-        self.canvas.draw()
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-
-    main = Window()
-    main.show()
-
+    app = QApplication([])
+    window = MainWindow()
+    window.show()
     sys.exit(app.exec_())
