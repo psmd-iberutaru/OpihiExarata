107652b1eb4a6bc6992b53f95dabc7f1ee2aa33e
Sparrow <psmd.iberutaru@gmail.com>
2022-04-15 20:05:54 -1000

    Got images loaded.

--- src/opihiexarata/gui/primary.py
+++ src/opihiexarata/gui/primary.py
@@ -2,19 +2,21 @@
 The primary GUI window.
 """
 
+import sys
+import random
+import os
+
 import PyQt6 as PyQt
 from PyQt6 import QtCore, QtWidgets, QtGui
 
-import sys
-import random
+import numpy as np
 
+import matplotlib.pyplot as plt
 import matplotlib.figure as mpl_figure
-
-# Using Qt backends.
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
 
-
+import opihiexarata
 import opihiexarata.library as library
 import opihiexarata.library.error as error
 import opihiexarata.library.hint as hint
@@ -23,12 +25,21 @@ import opihiexarata.gui as gui
 
 class OpihiPrimaryWindow(QtWidgets.QMainWindow):
     """The GUI that is responsible for interaction between the user and the
-    two primary Opihi solutions, the image (OpihiPreprocessSolution) and the 
+    two primary Opihi solutions, the image (OpihiPreprocessSolution) and the
     results (OpihiSolution).
+
+    Only non-GUI attributes are listed here.
+
+    Attributes
+    ----------
+    raw_fits_filename
+    process_fits_filename
+    preprocess_solution
+    opihi_solution
+
     """
 
-
-    def __init__(self):
+    def __init__(self) -> None:
         """The primary GUI window for OpihiExarata. This interacts directly
         with the total solution object of Opihi.
 
@@ -41,16 +52,29 @@ class OpihiPrimaryWindow(QtWidgets.QMainWindow):
         None
         """
         # Creating the GUI itself using the Qt framework and the converted
-        # Qt designer files.s
+        # Qt designer files.
         super(OpihiPrimaryWindow, self).__init__()
         self.ui = gui.qtui.Ui_PrimaryWindow()
         self.ui.setupUi(self)
+
+        # Establishing the defaults for all of the relevant attributes.
+        self.raw_fits_filename = None
+        self.process_fits_filename = None
+        self.preprocess_solution = None
+        self.opihi_solution = None
 
         # Preparing the new file buttons.
         self.__init_new_file_buttons()
 
         # Preparing the image area for Opihi sky images.
         self.__init_opihi_image()
+
+        # Preparing the preprocessing solution so that the raw files loaded
+        # into Exarata can be instantly turned into reduced images.
+        self.__init_preprocess_solution()
+
+        # All done.
+        return None
 
     def __init_new_file_buttons(self) -> None:
         """Assign the action bindings for the buttons which get new
@@ -65,11 +89,11 @@ class OpihiPrimaryWindow(QtWidgets.QMainWindow):
         None
         """
         # Assigning actions to the buttons.
-        self.ui.button_new_image_automatic.clicked.connect(
-            lambda: self.get_new_fits_image(filename_mode="automatic")
+        self.ui.push_button_new_image_automatic.clicked.connect(
+            self.__connect_new_fits_filename_automatic
         )
-        self.ui.button_new_image_manual.clicked.connect(
-            lambda: self.get_new_fits_image(filename_mode="manual")
+        self.ui.push_button_new_image_manual.clicked.connect(
+            self.__connect_new_fits_filename_manual
         )
 
     def __init_opihi_image(self) -> None:
@@ -85,27 +109,278 @@ class OpihiPrimaryWindow(QtWidgets.QMainWindow):
         -------
         None
         """
+        # Deriving the size of the image from the filler dummy image. The
+        # figure should be a square. (Height really is the primary concern.)
+        pix_to_in = lambda p: p / plt.rcParams["figure.dpi"]
+        dummy_edge_size = self.ui.dummy_opihi_image.height()
+        edge_size = pix_to_in(dummy_edge_size)
         # The figure, canvas, and navigation toolbar of the image plot
         # using a Matplotlib Qt widget backend. We will add these to the
         # layout later.
-        self._opihi_figure = mpl_figure.Figure()
+        fig, ax = plt.subplots(figsize=(edge_size, edge_size), constrained_layout=True)
+        self._opihi_figure = fig
+        self._opihi_axes = ax
         self._opihi_canvas = FigureCanvas(self._opihi_figure)
         self._opihi_nav_toolbar = NavigationToolbar(self._opihi_canvas, self)
 
-        # Just some button connected to `plot` method
-        self.button = QtWidgets.QPushButton("Plot")
-        self.button.clicked.connect(self.redraw_opihi_image)
+        # For ease of usage, a custom navigation bar coordinate formatter
+        # function/class is used.
+        class CoordinateFormatter:
+            """A simple function class to properly format the navigation bar
+            coordinate text. This assumes the current structure of the GUI."""
+
+            def __init__(self, gui_instance: OpihiPrimaryWindow) -> None:
+                self.gui_instance = gui_instance
+                return None
+
+            def __call__(self, x, y) -> str:
+                """The coordinate string going to be put onto the navigation
+                bar."""
+                x_index = int(x)
+                y_index = int(y)
+                coord_string = "Helllo [{x_int:d}, {y_int:d}]".format(
+                    x_int=x_index, y_int=y_index
+                )
+                print(coord_string)
+                return coord_string
+
+        # Assigning the coordinate formatter derived.
+        self._opihi_coordinate_formatter = CoordinateFormatter(self)
+        self._opihi_axes.format_coord = self._opihi_coordinate_formatter
+        self._opihi_axes.format_cursor_data = lambda s: "Poop"
+
+        # The push button for redrawing/refreshing the figure.
+        self.push_button_redraw_plot = QtWidgets.QPushButton("Refresh")
+        self.push_button_redraw_plot.clicked.connect(self.redraw_opihi_image)
 
         # Setting the layout, it is likely better to have the toolbar below
         # rather than above to avoid conflicts with the reset buttons in the
         # event of a misclick.
-        self.ui.image_vertical_layout.addWidget(self._opihi_canvas)
-        self.ui.image_vertical_layout.addWidget(self._opihi_nav_toolbar)
-        self.ui.image_vertical_layout.addWidget(self.button)
+        self.ui.vertical_layout_image.addWidget(self._opihi_canvas)
+        self.ui.vertical_layout_image.addWidget(self._opihi_nav_toolbar)
+        self.ui.vertical_layout_image.addWidget(self.push_button_redraw_plot)
         # Remove the dummy spacer otherwise it is just extra unneeded space.
-        self.ui.image_vertical_layout.removeWidget(self.ui.dummy_opihi_image)
+        self.ui.vertical_layout_image.removeWidget(self.ui.dummy_opihi_image)
         self.ui.dummy_opihi_image.deleteLater()
         self.ui.dummy_opihi_image = None
+        del self.ui.dummy_opihi_image
+        return None
+
+    def __init_preprocess_solution(self):
+        """Initialize the preprocessing solution. The preprocessing files
+        should be specified in the configuration file.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        None
+        """
+        # Using the configuration file to extract where the preprocessing
+        # filenames are to build the solution.
+        try:
+            preprocess = opihiexarata.OpihiPreprocessSolution(
+                mask_c_fits_filename=library.config.PREPROCESS_MASK_C_FITS_FILENAME,
+                mask_g_fits_filename=library.config.PREPROCESS_MASK_G_FITS_FILENAME,
+                mask_r_fits_filename=library.config.PREPROCESS_MASK_R_FITS_FILENAME,
+                mask_i_fits_filename=library.config.PREPROCESS_MASK_I_FITS_FILENAME,
+                mask_z_fits_filename=library.config.PREPROCESS_MASK_Z_FITS_FILENAME,
+                mask_1_fits_filename=library.config.PREPROCESS_MASK_1_FITS_FILENAME,
+                mask_2_fits_filename=library.config.PREPROCESS_MASK_2_FITS_FILENAME,
+                mask_3_fits_filename=library.config.PREPROCESS_MASK_3_FITS_FILENAME,
+                flat_c_fits_filename=library.config.PREPROCESS_FLAT_C_FITS_FILENAME,
+                flat_g_fits_filename=library.config.PREPROCESS_FLAT_G_FITS_FILENAME,
+                flat_r_fits_filename=library.config.PREPROCESS_FLAT_R_FITS_FILENAME,
+                flat_i_fits_filename=library.config.PREPROCESS_FLAT_I_FITS_FILENAME,
+                flat_z_fits_filename=library.config.PREPROCESS_FLAT_Z_FITS_FILENAME,
+                flat_1_fits_filename=library.config.PREPROCESS_FLAT_1_FITS_FILENAME,
+                flat_2_fits_filename=library.config.PREPROCESS_FLAT_2_FITS_FILENAME,
+                flat_3_fits_filename=library.config.PREPROCESS_FLAT_3_FITS_FILENAME,
+                bias_fits_filename=library.config.PREPROCESS_BIAS_FITS_FILENAME,
+                dark_current_fits_filename=library.config.PREPROCESS_DARK_CURRENT_FITS_FILENAME,
+                linearity_fits_filename=library.config.PREPROCESS_LINEARITY_FITS_FILENAME,
+            )
+        except Exception:
+            # Something failed with making the preprocess solution, a
+            # configuration file issue is likely the reason.
+            # TODO
+            preprocess = None
+        finally:
+            self.preprocess_solution = preprocess
+        # All done.
+        return None
+
+    def __connect_new_fits_filename_automatic(self):
+        """The automatic method relying on earliest fits file avaliable in
+        the expected directory. This function is a connected function action to
+        a button in the GUI.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        None
+        """
+        # TODO
+        new_fits_filename = "pie"
+
+        # If the user did not provide a file to enter, there is nothing to be
+        # changed.
+        if os.path.isfile(new_fits_filename):
+            # Assign the new fits filename.
+            self.raw_fits_filename = os.path.abspath(new_fits_filename)
+        else:
+            # Exit!
+            return None
+
+        # Process the file first so that what the user sees is closer to
+        # what it really is.
+
+        # Load up the new file.
+        self._load_fits_file(fits_filename=self.raw_fits_filename)
+        # All done.
+        return None
+
+    def __connect_new_fits_filename_manual(self):
+        """The manual method relying on earliest fits file avaliable in
+        the expected directory. This function is a connected function action to
+        a button in the GUI.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        None
+        """
+        # Ask the user for the filename via a dialog.
+        new_fits_filename, __ = QtWidgets.QFileDialog.getOpenFileName(
+            parent=self,
+            caption="Open Opihi Image",
+            directory="./",
+            filter="FITS Files (*.fits)",
+        )
+        # If the user did not provide a file to enter, there is nothing to be
+        # changed.
+        if os.path.isfile(new_fits_filename):
+            # Assign the new fits filename.
+            self.raw_fits_filename = os.path.abspath(new_fits_filename)
+        else:
+            # Exit!
+            return None
+
+        # Process the file first so that what the user sees is closer to
+        # what it really is.
+
+        # Load up the new file.
+        self._load_fits_file(fits_filename=self.raw_fits_filename)
+        # All done.
+        return None
+
+    def _preprocess_fits_file(
+        self, raw_filename: str, process_filename: str = None
+    ) -> str:
+        """Using the provided filename, preprocess the fits file listed.
+
+        This is used primarily during the loading of a new fits file.
+
+        Parameters
+        ----------
+        raw_filename : string
+            The filename of the raw fits file to be preprocessed.
+        process_filename : string, default = None
+            The filename where the processed fits file will be saved to. If
+            None, it defaults to a sensible naming convention to distinguish
+            the two.
+
+        Returns
+        -------
+        process_filename : string
+            The filename of the preprocessed file is returned, this is just
+            in case the value was derived from the naming convention. This is
+            an absolute path.
+        """
+        # Deriving saving location of the processed fits file.
+        if isinstance(process_filename, str):
+            process_filename = os.path.abspath(process_filename)
+        else:
+            # Use a sensible naming convention to distinguish between the raw
+            # and processed file.
+            # TODO
+            process_filename = "./process.fits"
+
+        # Saving for later.
+        self.process_fits_filename = process_filename
+
+        # Process the fits file.
+        __ = self.preprocess_solution.preprocess_fits_file(
+            raw_filename=raw_filename, out_filename=process_filename
+        )
+        # All done.
+        return process_filename
+
+    def _load_fits_file(self, fits_filename: str) -> None:
+        """Load a fits file into the GUI to derive all of the solutions needed.
+
+        This loads the fits file into an OpihiSolution class for which this
+        GUI interacts with and derives the information from.
+
+        Parameters
+        ----------
+        fits_filename : str
+            The fits filename which will be loaded.
+
+        Returns
+        -------
+        None
+        """
+
+        # Although the OpihiSolution could derive these values from the
+        # header of the filename, the solution class is built to be general.
+        filter_name = "g"
+        exposure_time = 10
+        observing_time = 100
+        asteroid_name = None
+        asteroid_location = None
+        asteroid_history = None
+
+        # Creating the solution from the data.
+        opihi_solution = opihiexarata.OpihiSolution(
+            fits_filename=fits_filename,
+            filter_name=filter_name,
+            exposure_time=exposure_time,
+            observing_time=observing_time,
+            asteroid_name=asteroid_name,
+            asteroid_location=asteroid_location,
+            asteroid_history=asteroid_history,
+        )
+        self.opihi_solution = opihi_solution
+
+        # Because a new image was loaded, the previous values and other
+        # information derived from the last image is invalid, reset and replot.
+        self.reset_dynamic_label_text()
+        self.redraw_opihi_image()
+        return None
+
+    def reset_dynamic_label_text(self) -> None:
+        """Reset all of the dynamic label text, this is traditionally done
+        just before a new image is going to be introduced.
+
+        This resets the text back to their defaults.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        None
+        """
         return None
 
     def redraw_opihi_image(self) -> None:
@@ -120,69 +395,26 @@ class OpihiPrimaryWindow(QtWidgets.QMainWindow):
         -------
         None
         """
-        # random data
-        data = [random.random() for i in range(10)]
+        # Clear the information before replotting, it is easier just to draw
+        # it all again.
+        self._opihi_axes.clear()
 
-        # instead of ax.hold(False)
-        self._opihi_figure.clear()
+        # Attempt to plot the image data if it exists.
+        if self.opihi_solution is not None:
+            image = self._opihi_axes.imshow(self.opihi_solution.data)
+            image.format_cursor_data = lambda s: ""
 
-        # create an axis
-        ax = self._opihi_figure.add_subplot(111)
 
-        # discards the old graph
-        # ax.hold(False) # deprecated, see above
+        # TESTING
+        rand = np.random.rand(5)
+        self._opihi_axes.plot(rand, 1 / rand)
 
-        # plot data
-        ax.plot(data, "*-")
-
-        # A tight layout to improve realestate efficiency.
-        self._opihi_figure.tight_layout()
+        # Make sure the coordinate formatter does not change.
+        self._opihi_axes.format_coord = self._opihi_coordinate_formatter
+        self._opihi_axes.format_cursor_data = lambda s: "Poop"
         # Update and redraw the image via redrawing the canvas.
         self._opihi_canvas.draw()
         return None
-
-    def get_new_fits_image(self, filename_mode: str = "automatic") -> str:
-        """Automatically get the most recent file taken from the area where the
-        fits files are to be stored.
-
-        Parameters
-        ----------
-        filename_mode : string
-            This function can get the new image filename automatically or
-            manually.
-
-                - `automatic` : Fetch the most recent image from the
-                  configured directory automatically.
-                - `manual` : Have the user select the file using a file
-                  dialog pop up.
-
-        Returns
-        -------
-        new_image : str
-            The absolute path of the new fits file.
-        """
-
-        def _get_new_fits_filename(mode:str):
-            # Different methods based on the different modes.
-            filename_mode = filename_mode.casefold()
-            if filename_mode == "automatic":
-                # Find the file name automatically using the most recent image.
-                # TODO
-                new_filename = "pie"
-            elif filename_mode == "manual":
-                # Use a file dialog box to get the file name. We do not need the
-                # filter information.
-                new_filename, __ = QtWidgets.QFileDialog.getOpenFileName(
-                    parent=self,
-                    caption="Open Opihi Image",
-                    directory="./",
-                    filter="FITS Files (*.fits)",
-                )
-            else:
-                raise error.InputError(
-                    "The filename mode of getting the filename is not supported."
-                )
-            return new_filename
 
 
 def main():
