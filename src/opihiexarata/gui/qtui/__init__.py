a9dba09d8830672b42e2f6b5e048efe81f955cde
Sparrow <psmd.iberutaru@gmail.com>
2022-06-13 15:38:50 -1000

    Adding automatic mode GUI.

--- src/opihiexarata/gui/qtui/__init__.py
+++ src/opihiexarata/gui/qtui/__init__.py
@@ -2,3 +2,5 @@
 
 from opihiexarata.gui.qtui.qtui_manual import Ui_ManualWindow
 from opihiexarata.gui.qtui.qtui_selector import Ui_SelectorWindow
+
+from opihiexarata.gui.qtui.qtui_automatic import Ui_AutomaticWindow
