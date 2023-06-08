13017710b07343c9a25d999755c0669106e3ef6d
Sparrow <psmd.iberutaru@gmail.com>
2023-06-07 17:02:29 -1000

    Weird left over files.

--- qtui_automatic.py
+++
@@ -1,4 +0,0 @@
-Error: File '.\automatic.ui' is not valid
-
-
-while executing 'C:\Users\psmdi\AppData\Local\Programs\Python\Python311\Lib\site-packages\PySide6\uic -g python .\automatic.ui'
--- qtui_selector.py
+++
@@ -1,4 +0,0 @@
-Error: File '.\selector.ui' is not valid
-
-
-while executing 'C:\Users\psmdi\AppData\Local\Programs\Python\Python311\Lib\site-packages\PySide6\uic -g python .\selector.ui'
