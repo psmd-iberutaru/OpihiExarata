0b58480e3429d5e6c0e8771eced6811693c50850
Sparrow <psmd.iberutaru@gmail.com>
2026-03-05 17:12:56 -1000

    Change for Wikipedia test.

--- tests/test_library/test_library_http.py
+++ tests/test_library/test_library_http.py
@@ -20,10 +20,14 @@ def test_download_file_from_url() -> None:
     # and they have nice public domain images. Sparrow likes this logo.
     image_url = "https://upload.wikimedia.org/wikipedia/commons/6/6d/Seal_of_Cybersecurity_and_Infrastructure_Security_Agency.svg"
     save_path = "./cisa_seal.svg"
+    # For the Wikipedia link, they require a User agent.
+    http_headers = {
+        "User-Agent":"Mozilla/5.0 (X11; Linux i686; rv:148.0) Gecko/20100101 Firefox/148.0",
+    }
     # Try to download the image.
     try:
         opihiexarata.library.http.download_file_from_url(
-            url=image_url, filename=save_path
+            url=image_url, filename=save_path, http_headers=http_headers
         )
     except:
         assert_message = "Could not download the file from the test link."
